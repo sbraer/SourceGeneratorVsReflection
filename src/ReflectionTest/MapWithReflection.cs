3c7ae78ddb34289ba7127fd382f8e0097628afdf
az <az@DESKTOP-I4A2J2R>
2024-10-21 21:37:59 +0200

    optimizatio

--- src/ReflectionTest/MapWithReflection.cs
+++ src/ReflectionTest/MapWithReflection.cs
@@ -14,6 +14,11 @@ public static class MapWithReflection
         {
             // read header
             string[] headers = allRowsFile[0].Split(',');
+            List<PropertyInfo> propertiesInfo = [];
+            for (int i = 0; i < headers.Length; i++)
+            {
+                propertiesInfo.Add(typeof(RandomPropertiesClass).GetProperty(headers[i])!);
+            }
 
             for (var counter = 1; counter < allRowsFile.Length; counter++)
             {
@@ -21,32 +26,30 @@ public static class MapWithReflection
                 var reader = allRowsFile[counter].AsSpan();
 
                 RandomPropertiesClass item = new RandomPropertiesClass();
+
                 var index = reader.IndexOf(',');
                 for (int i = 0; i < headers.Length; i++)
                 {
-                    PropertyInfo prop = typeof(RandomPropertiesClass).GetProperty(headers[i])!;
-                    if (prop is not null)
+                    PropertyInfo prop = propertiesInfo[i];
+                    object value;
+                    
+                    // Se non è l'ultimo elemento, fai lo slicing fino all'indice della virgola
+                    if (i != headers.Length - 1)
                     {
-                        object value;
-                        
-                        // Se non è l'ultimo elemento, fai lo slicing fino all'indice della virgola
-                        if (i != headers.Length - 1)
-                        {
-                            value = Convert.ChangeType(reader.Slice(0, index).ToString(), prop.PropertyType);
-                            reader = reader.Slice(index + 1);  // Aggiorna il reader
-                            index = reader.IndexOf(',');       // Trova il prossimo indice della virgola
-                        }
-                        else
-                        {
-                            // Se è l'ultimo elemento, prendi tutto il resto del reader
-                            value = Convert.ChangeType(reader.Slice(0).ToString(), prop.PropertyType);
-                        }
-                        
-                        // Imposta il valore sulla proprietà
-                        prop.SetValue(item, value);
+                        value = Convert.ChangeType(reader.Slice(0, index).ToString(), prop.PropertyType);
+                        reader = reader.Slice(index + 1);  // Aggiorna il reader
+                        index = reader.IndexOf(',');       // Trova il prossimo indice della virgola
                     }
-
+                    else
+                    {
+                        // Se è l'ultimo elemento, prendi tutto il resto del reader
+                        value = Convert.ChangeType(reader.Slice(0).ToString(), prop.PropertyType);
+                    }
+                    
+                    // Imposta il valore sulla proprietà
+                    prop.SetValue(item, value);
                 }
+
                 loadedData.Add(item);
             }
 
